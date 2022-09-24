2d5aa1f8fb6d8b9970766391041243325cb92c66
Fanomenz4ntsoa <voary.randrianarison@esti.mg>
2022-09-24 12:30:03 +0200

    feat: adding routes

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-"# Restful_Api" 
+Building a restful api uing reactPHP
+
+/* 
+  * Mise en place d'un serveur en fonctionnement
+  qui traitera les demandes entrantes.
+  Serveur HTTP vide.
+  * configuration des routes avec l'utilisation
+  de FastRoute 
+
+*/
--- index.php
+++ index.php
@@ -1,23 +1,55 @@
 /* 
-  Mise en place d'un serveur en fonctionnement
+  * Mise en place d'un serveur en fonctionnement
   qui traitera les demandes entrantes.
   Serveur HTTP vide.
+  * configuration des routes avec l'utilisation
+  de FastRoute 
+
 */
 
 <?php
 require('vendor/autoload.php');
 
+use Psr\Http\Message\ServerRequestInterface;
 use React\MySQL\Factory;
 use React\Http\Message\Response;
 use React\Http\Server;
 
 $loop = \React\EventLoop\Factory::create();
 
-$hello = function() {
-  return new Response(200,['content-type' => 'application/json'], json_encode(['message' => 'Hello World']));
-};
+$dispatcher = \FastRoute\simpleDispatcher(function(\FastRoute\RouteCollector $routes) {
+  $routes->get('/products', function(ServerRequestInterface $request){
+    return new Response(
+      200,
+      ['Content-type' => "application/json "],
+      json_encode(['message' => 'GET request to /products'])
+    );
+  });
+  $routes->post('/products', function(ServerRequestInterface $request){
+    return new Response(
+      200,
+      ['Content-type' => "application/json "],
+      json_encode(['message' => 'POST request to /products'])
+    );
+  });  
+});
 
-$server = new Server($hello);
+$server = new Server(function(ServerRequestInterface $request) use ($dispatcher) {
+  $routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getUri()->getPath());
+
+  switch ($routeInfo[0]) {
+    case \FastRoute\Dispatcher::NOT_FOUND;
+      return new Response(404, ['Content-type' => 'text/plain'], "Not Found");
+    case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED;
+      return new Response(405, ['Content-type' => 'text/plain'], 'Method not allowed');
+    case \FastRoute\Dispatcher::FOUND;
+      return $routeInfo[1]($request);
+
+    throw new Exception("Something went wrong with routing");
+    
+  }
+});
+
 $socket = new \React\Socket\Server('127.0.0.1:8080', $loop);
 $server->listen($socket);
 
